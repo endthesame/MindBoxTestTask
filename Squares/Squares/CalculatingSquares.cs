722ac4314409cec63beb36ba316d7b4d88563681
endthesame <endthesame@gmail.com>
2022-04-12 21:27:08 +0300

    Squares
    added test task with squares to repository

---
+++ Squares/Squares/CalculatingSquares.cs
@@ -0,0 +1,37 @@
+﻿namespace Squares
+{
+    public class CalculatingSquares
+    {
+        public static double SquareCirlce(double radius)
+        {
+            if (radius < 0)
+                throw new ArgumentOutOfRangeException(nameof(radius), "Radius value is not corrected, raadius must be => 0");
+
+            double sqaure = Math.PI* radius *radius;
+            return sqaure;
+        }
+
+        public static double SquareTriangle(double firstSide, double secondSide, double thirdSide)
+        {
+            if (firstSide <=0 || secondSide <= 0 || thirdSide <= 0)
+                throw new ArgumentOutOfRangeException("one of the sides value is not corrected, side must be > 0");
+
+            double p = (firstSide + secondSide + thirdSide) / 2;
+            return Math.Sqrt(p * (p - firstSide) * (p - secondSide) * (p - thirdSide));
+        }
+
+        public static bool IsTriangleRectangular(double firstSide, double secondSide, double thirdSide) // Не уверен нужно ли было создавать отдельный метод или нужно писать проверку в методе SquareTriangle
+        {
+            bool result = false;
+
+            if (firstSide <= 0 || secondSide <= 0 || thirdSide <= 0)
+                throw new ArgumentOutOfRangeException("one of the sides value is not corrected, side must be > 0");
+
+            if ((firstSide * firstSide == secondSide * secondSide + thirdSide * thirdSide) ||
+                (secondSide * secondSide == firstSide * firstSide + thirdSide * thirdSide) ||
+                (thirdSide * thirdSide == firstSide * firstSide + secondSide * secondSide)) { result = true; }
+
+            return result;
+        }
+    }
+}
---
+++ Squares/SquaresTests/CalculatingSquaresTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Squares;
+
+namespace SquaresTests
+{
+    [TestClass]
+    public class CalculatingSquaresTests
+    {
+        [TestMethod]
+        public void SquareCirlce_PositiveRadius()
+        {
+            double radius = 5f;
+            double expected = 78.539f;
+
+            double actual = CalculatingSquares.SquareCirlce(radius);
+            Assert.AreEqual(expected, actual, 0.001);
+        }
+
+        [TestMethod]
+        public void SquareCirlce_NegativeRadius()
+        {
+            double radius = -5f;
+
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => CalculatingSquares.SquareCirlce(radius));
+        }
+
+        [TestMethod]
+        public void SquareTriangle_AllPositiveValues()
+        {
+            double firstSide = 2.5f;
+            double secondSide = 3.3f;
+            double thirdSide = 4f;
+            double expected = 4.115f;
+
+            double actual = CalculatingSquares.SquareTriangle(firstSide, secondSide, thirdSide);
+            Assert.AreEqual(expected, actual, 0.001);
+        }
+
+        [TestMethod]
+        public void SquareTriangle_ZeroSide()
+        {
+            double firstSide = 2.5f;
+            double secondSide = 0f;
+            double thirdSide = 4f;
+
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => CalculatingSquares.SquareTriangle(firstSide, secondSide, thirdSide));
+        }
+
+        [TestMethod]
+        public void SquareTriangle_NegativeSide()
+        {
+            double firstSide = 2.5f;
+            double secondSide = 3f;
+            double thirdSide = -4.3f;
+
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => CalculatingSquares.SquareTriangle(firstSide, secondSide, thirdSide));
+        }
+
+
+
+    }
+}
